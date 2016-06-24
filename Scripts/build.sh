c3c2003fa0a5525cd9e0e120a3ad3eb7f294b4b4
Thomas Pakh <thomas.pakh@gmail.com>
2016-06-23 23:10:05 -0400

    Improved build directories.

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -5,21 +5,23 @@
 #   -batchmode \
 #   -nographics \
 #   -silent-crashes \
-#   -logFile $TRAVIS_BUILD_DIR/unity.log \
-#   -projectPath $TRAVIS_BUILD_DIR \
-#   -buildWindowsPlayer "$TRAVIS_BUILD_DIR/Build/windows/$PROJECT_NAME-x86.exe" \
+#   -logFile $UNITY_BUILD_DIR/unity.log \
+#   -projectPath $UNITY_BUILD_DIR \
+#   -buildWindowsPlayer "$UNITY_BUILD_DIR/windows/$PROJECT_NAME-x86.exe" \
 #   -quit
 
-mkdir -p $TRAVIS_BUILD_DIR
+UNITY_BUILD_DIR=$TRAVIS_BUILD_DIR/Build
+
+mkdir -p $UNITY_BUILD_DIR
 
 echo "Attempting to build $PROJECT_NAME for Windows (64-bit)"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
   -nographics \
   -silent-crashes \
-  -logFile $TRAVIS_BUILD_DIR/unity.log \
+  -logFile $UNITY_BUILD_DIR/unity.log \
   -projectPath $TRAVIS_BUILD_DIR \
-  -buildWindowsPlayer64 "$TRAVIS_BUILD_DIR/Build/windows/$PROJECT_NAME-x86_64.exe" \
+  -buildWindowsPlayer64 "$UNITY_BUILD_DIR/windows/$PROJECT_NAME-x86_64.exe" \
   -quit
 
 echo "Attempting to build $PROJECT_NAME for OS X"
@@ -27,20 +29,20 @@ echo "Attempting to build $PROJECT_NAME for OS X"
   -batchmode \
   -nographics \
   -silent-crashes \
-  -logFile $TRAVIS_BUILD_DIR/unity.log \
+  -logFile $UNITY_BUILD_DIR/unity.log \
   -projectPath $TRAVIS_BUILD_DIR \
-  -buildOSXUniversalPlayer "$TRAVIS_BUILD_DIR/Build/osx/$PROJECT_NAME.app" \
+  -buildOSXUniversalPlayer "$UNITY_BUILD_DIR/osx/$PROJECT_NAME.app" \
   -quit
 
-echo "Attempting to build $PROJECT_NAME for Linux"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-  -batchmode \
-  -nographics \
-  -silent-crashes \
-  -logFile $TRAVIS_BUILD_DIR/unity.log \
-  -projectPath $TRAVIS_BUILD_DIR \
-  -buildLinuxUniversalPlayer "$TRAVIS_BUILD_DIR/Build/linux/$PROJECT_NAME" \
-  -quit
+# echo "Attempting to build $PROJECT_NAME for Linux"
+# /Applications/Unity/Unity.app/Contents/MacOS/Unity \
+#   -batchmode \
+#   -nographics \
+#   -silent-crashes \
+#   -logFile $UNITY_BUILD_DIR/unity.log \
+#   -projectPath $TRAVIS_BUILD_DIR \
+#   -buildLinuxUniversalPlayer "$UNITY_BUILD_DIR/linux/$PROJECT_NAME" \
+#   -quit
 
 echo 'Logs from build'
-cat $TRAVIS_BUILD_DIR/unity.log
+cat $UNITY_BUILD_DIR/unity.log
