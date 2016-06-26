0ea31e8b3b2fd1b18a4748f5a8d2a4b98c286e63
Thomas Pakh <thomas.pakh@gmail.com>
2016-06-26 00:27:21 -0400

    Fixed aspect ratio.

---
+++ Assets/Scripts/AspectRatio.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Camera))]
+public class AspectRatio : MonoBehaviour {
+
+	public float targetAspect;
+
+	void Start () 
+	{
+		float windowAspect = (float) Screen.width / (float) Screen.height;
+		float scaleHeight = windowAspect / targetAspect;
+		Camera camera = GetComponent<Camera>();
+
+		if (scaleHeight < 1.0f)
+			camera.orthographicSize = camera.orthographicSize / scaleHeight;
+	}
+}
